171869ec8f14329a8dbd029ac65cc98f6396569b
Mani Makaremi <manimakaremi@Manis-MacBook-Pro.local>
2019-03-12 16:09:21 -0700

    Updates

---
+++ precache-manifest.ab312653635ca01ada4881298938e3ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e544eacf1332ac93f23e",
+    "url": "/Comp680/tree/master/web-ui/static/css/main.da0c4f82.chunk.css"
+  },
+  {
+    "revision": "e544eacf1332ac93f23e",
+    "url": "/Comp680/tree/master/web-ui/static/js/main.e544eacf.chunk.js"
+  },
+  {
+    "revision": "f3fb817465e673217106",
+    "url": "/Comp680/tree/master/web-ui/static/js/runtime~main.f3fb8174.js"
+  },
+  {
+    "revision": "9f737a589c9dd984e3de",
+    "url": "/Comp680/tree/master/web-ui/static/css/2.eeab12cf.chunk.css"
+  },
+  {
+    "revision": "9f737a589c9dd984e3de",
+    "url": "/Comp680/tree/master/web-ui/static/js/2.9f737a58.chunk.js"
+  },
+  {
+    "revision": "38906b080dc2e851e868adc5c876c041",
+    "url": "/Comp680/tree/master/web-ui/index.html"
+  }
+];
