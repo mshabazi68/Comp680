2155ba15e6c02b2795e3169f65f7a144d48b6198
Mohammadreza Shahbazi <mshabazi68@gmail.com>
2019-03-13 00:09:51 -0700

    Add files via upload
    to run the app please run : 
    npm install --save react-google-maps # or
    yarn add react-google-maps

---
+++ my-moapp/src/index.js
@@ -0,0 +1,60 @@
+
+ /*global google*/
+import React from "react";
+import ReactDOM from "react-dom";
+//import { DirectionsRenderer } from "react-google-maps";
+import { compose, withProps, lifecycle } from "recompose";
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  DirectionsRenderer,
+
+} from "react-google-maps";
+
+//const google=window.google
+//import { DirectionsRenderer } from "react-google-maps";
+
+const MapWithADirectionsRenderer = compose(
+  withProps({
+    googleMapURL:
+      "https://maps.googleapis.com/maps/api/js?key=AIzaSyAcQjrfAudzl6Ton7GA7D-gVqOINMFE7ns&v=3.exp&libraries=geometry,drawing,places",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `400px` }} />,
+    mapElement: <div style={{ height: `100%` }} />
+  }),
+  withScriptjs,
+  withGoogleMap,
+
+  lifecycle({
+    componentDidMount() {
+      const DirectionsService = new google.maps.DirectionsService();
+
+      DirectionsService.route(
+        {
+          origin: new google.maps.LatLng(40.712776, -74.005974),
+          destination: new google.maps.LatLng(34.052235, -118.243683),
+          travelMode: google.maps.TravelMode.DRIVING
+        },
+        (result, status) => {
+          if (status === google.maps.DirectionsStatus.OK) {
+            this.setState({
+              directions: result
+            });
+          } else {
+            console.error(`error fetching directions ${result}`);
+          }
+        }
+      );
+    }
+  })
+)(props => (
+  <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
+    {props.directions && <DirectionsRenderer directions={props.directions} />}
+  </GoogleMap>
+));
+
+ReactDOM.render(
+  < MapWithADirectionsRenderer directions />,
+  document.getElementById("root")
+);
